ffb21904264112c01c6a48ea8968a752a87363f4
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-08-08 15:17:04 +0530

    feat: add hover effect to change grid's box colors

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+function hover(box) {
+    box.style.backgroundColor = "black"; // Adds on the existing one. Don't do setAttr as it removes the previously set background color.
+}
+
 const grid = document.querySelector("#grid");
 
 for (let i = 0; i < 256; i++) {
@@ -5,5 +9,8 @@ for (let i = 0; i < 256; i++) {
     // In each row and column, there are going to be 16 squares. The size must be divided equally (100/16 = 6.25)
     // Use outline and not border, as border takes space from the size of the square.
     box.setAttribute("style", "height: 6.25%; width: 6.25%; outline: 1px solid black;"); 
+    box.addEventListener("mouseenter", function() {
+        hover(box);
+    });
     grid.appendChild(box);
 }
