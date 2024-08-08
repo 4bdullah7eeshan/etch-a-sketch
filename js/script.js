30235f8043b44464110b166e6e2d6ba85439757e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-08-08 15:08:03 +0530

    docs: add comments to explain grid creation logic

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const grid = document.querySelector("#grid");
 
 for (let i = 0; i < 256; i++) {
     const box = document.createElement("div");
-    box.setAttribute("style", "height: 6.25%; width: 6.25%; outline: 1px solid black;");
+    // In each row and column, there are going to be 16 squares. The size must be divided equally (100/16 = 6.25)
+    // Use outline and not border, as border takes space from the size of the square.
+    box.setAttribute("style", "height: 6.25%; width: 6.25%; outline: 1px solid black;"); 
     grid.appendChild(box);
 }
